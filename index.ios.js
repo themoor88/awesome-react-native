74e923197aab915f13c16f64eaded94be51e5408
WajidKagzi <wjkagzi@gmail.com>
2015-10-13 21:43:44 -0400

    Adding more React components.

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
 
-var TinderHouse = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class HelloWorld extends React.Component {
+  render () {
+    return <React.Text style={styles.text}>Hello World(Again)</React.Text>;
+  }
+}
 
-AppRegistry.registerComponent('TinderHouse', () => TinderHouse);
+class TinderHouseApp extends React.Component {
+  render () {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Tinder House' ,
+          component: HelloWorld,
+        }}/>
+    );
+  }
+}
+
+React.AppRegistry.registerComponent('TinderHouse', function() { return TinderHouseApp });
